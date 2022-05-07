e0f9adf0981cbc85c883a20596c97043607a3f0c
vihanga <liyanage.vihanga99@gmail.com>
2022-05-08 02:43:01 +0530

    Added Cores support

--- src/main/java/com/driw/assingment/shop/driw_shop/DriwShopApplication.java
+++ src/main/java/com/driw/assingment/shop/driw_shop/DriwShopApplication.java
@@ -2,10 +2,31 @@ package com.driw.assingment.shop.driw_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DriwShopApplication {
-
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        String accessControlOriginHeader="Access-Control-Allow-Origin";
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://cdn1.iconfinder.com"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", accessControlOriginHeader, "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                accessControlOriginHeader, accessControlOriginHeader, "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
     public static void main(String[] args) {
         SpringApplication.run(DriwShopApplication.class, args);
     }

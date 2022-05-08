12c0f292d2d7a99caa99231b2ef9dfef9d55428d
vihanga <liyanage.vihanga99@gmail.com>
2022-05-08 17:47:27 +0530

    implemented cards

--- driw-shop-ui/src/pages/PriceCatelog/PriceCatelog.jsx
+++ driw-shop-ui/src/pages/PriceCatelog/PriceCatelog.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import './PriceCatelog.css'
+import React from "react";
+import "./PriceCatelog.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Productcard from "../../components/productCard/ProductCard";
+
 const PriceCatelog = () => {
-    return (
-        <div className='container'>
-            This is the PriceCatelog
-        </div>
-    );
-}
+  const [products, setProducts] = useState([]);
+  // const [seproductslectedItemId, setSelectedItemId] = useState(0);
+  // const [qty, setQty] = useState("");
+  // const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/v1/product")
+      .then((response) => {
+        setProducts(response.data);
+        console.log(products);
+      })
+      .catch((err) => {
+        setProducts([]);
+        console.error(err);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="container">
+      {/* {JSON.stringify(products)} */}
+          {products.map((product) => (
+            <Productcard productName={product.productName} cartonPrice={product.cartonPrice} />
+          ))}      
+    </div>
+  );
+};
 
 export default PriceCatelog;
